47eb41906917a3bd3121ea4e67a3efd78255f18e
nalanolgun <nalanolgun3@gmail.com>
2020-11-24 20:33:10 +0100

    1.islem

--- src/test.java
+++ src/test.java
@@ -2,4 +2,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("1.islem");
     }
+    public  static  void yeniMetod()
+    {
+        System.out.println("yeni metodum hazir");
+        System.out.println("sepete ekle kisimlari burasi ");
+    }
 }
